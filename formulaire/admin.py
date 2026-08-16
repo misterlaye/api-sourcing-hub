8da6a59f3c3be5615ef93907ca53a525fb6e43dc
bineta66 <binetabadadiane6@gmail.com>
2026-08-16 14:17:43 +0000

    creation des routes et gere aussi les filtres sur le admin

--- formulaire/admin.py
+++ formulaire/admin.py
@@ -1,3 +1,95 @@
 from django.contrib import admin
+from .models import ( Formulaire,SectionFormulaire,Question,OptionQuestion,
+)
+@admin.register(Formulaire)
+class FormulaireAdmin(admin.ModelAdmin):
+    """
+    Configuration du formulaire dans Django Admin.
+    """
+    list_display = (
+        "titre",
+        "campagne",
+        "publie",
+        "actif",
+        "date_creation",
+    )
+    list_filter = (
+        "publie",
+        "actif",
+    )
+    search_fields = (
+        "titre",
+        "description",
+    )
+    ordering = (
+        "-date_creation",
+    )
+@admin.register(SectionFormulaire)
+class SectionFormulaireAdmin(admin.ModelAdmin):
+    """
+    Configuration des sections dans Django Admin.
+    """
+    list_display = (
+        "titre",
+        "formulaire",
+        "ordre",
+        "date_creation",
+    )
+    list_filter = (
+        "formulaire",
+    )
+    search_fields = (
+        "titre",
+        "description",
+    )
+    ordering = (
+        "formulaire",
+        "ordre",
+    )
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Configuration des questions dans Django Admin.
+    """
+    list_display = (
+        "texte",
+        "section",
+        "type_question",
+        "obligatoire",
+        "ordre",
+    )
+    list_filter = (
+        "type_question",
+        "obligatoire",
+    )
+    search_fields = (
+        "texte",
+        "description",
+    )
+    ordering = (
+        "section",
+        "ordre",
+    )
 
-# Register your models here.
+@admin.register(OptionQuestion)
+class OptionQuestionAdmin(admin.ModelAdmin):
+    """
+    Configuration des options dans Django Admin.
+    """
+    list_display = (
+        "texte",
+        "valeur",
+        "question",
+        "ordre",
+    )
+    list_filter = (
+        "question",
+    )
+    search_fields = (
+        "texte",
+        "valeur",
+    )
+    ordering = (
+        "question",
+        "ordre",
+    )
